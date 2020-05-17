fd1cddf32132f0b40bc8bf31b8113efbf688125a
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-05-17 15:16:04 +0100

    add put, delete and validation by joi

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const _ = require('lodash');
+const Joi = require('joi');
 const port = global.process.env.PORT || 3000;
 
 const app = express();
@@ -9,6 +11,15 @@ var students = [
     {id: 3, name : 'Student 3', age : 25},
 ];
 
+const validation_schema = {
+    name : Joi.string().min(3).max(12).required(),
+    age: Joi.number().positive().required()
+}
+
+const validation_schema_put = {
+    name : Joi.string().min(3).max(12),
+    age: Joi.number().positive()
+}
 app.get('/api/students',(req,res)=>{
     res.send(students);
 });
@@ -23,6 +34,9 @@ app.get('/api/students/id/:id',(req,res)=>{
 });
 
 app.post('/api/students',(req,res)=>{
+    const result = Joi.validate(req.body,validation_schema);
+    if(result.error)
+        return res.status(400).send(result.error.details[0].message);
     const student = {
         id : students[students.length-1].id+1,//auto increment
         name: req.body.name,
@@ -31,5 +45,29 @@ app.post('/api/students',(req,res)=>{
     students.push(student);
     res.send(students);
 });
+
+app.delete('/api/students/id/:id',(req,res)=>{
+    if(isNaN(parseInt(req.params.id)))
+        return res.status(400).send(` ${req.params.id} is not a valid ID.`)
+    const student = students.find(s => s.id === parseInt(req.params.id));
+    if(!student)
+        return res.status(404).send(`Student with id : ${req.params.id} not found.`)
+    students = students.filter(s => s.id !== student.id);
+    res.send(student);
+});
+
+app.put('/api/students/id/:id',(req,res)=>{
+    const result = Joi.validate(req.body,validation_schema_put);
+    if(result.error)
+        return res.status(400).send(result.error.details[0].message);
+    if(isNaN(parseInt(req.params.id)))
+        return res.status(400).send(` ${req.params.id} is not a valid ID.`)
+    let student = students.find(s => s.id === parseInt(req.params.id));
+    if(!student)
+        return res.status(404).send(`Student with id : ${req.params.id} not found.`)
+    student = _.merge(student, req.body);
+    res.send(student);
+});
+
 
 app.listen(port, ()=> console.log(`Server run on ${port}.`));
